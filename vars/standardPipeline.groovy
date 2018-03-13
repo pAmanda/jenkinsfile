4c40efc1ca268520935ff27722ed667d9d450815
Admanda Vieira Pires <amanda.pires@mac-17.local>
2018-03-13 19:19:53 -0300

    Changes...

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -11,7 +11,7 @@ def call(body) {
 
             try {
                 stage ('Clone') {
-                    sh "echo 'Oieeee...'"
+                    sh "echo 'Oiee...'"
                     sh 'printenv'
                     checkout scm
                 }
