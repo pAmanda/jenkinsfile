88105acdf2f9266985fb00c88d335ce9f98ad1e5
Admanda Vieira Pires <amanda.pires@mac-17.local>
2018-03-13 17:41:48 -0300

    Adding variables

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -15,6 +15,7 @@ def call(body) {
                 }
                 stage ('Build') {
                     sh "echo 'building ${config.projectName} ...'"
+                    sh "echo 'variable = ${config.xpto} e ${config.version}'"
                     sh 'mvn clean install -Dmaven.test.skip=true -Dmaven.javadoc.skip=true'
                 }
                 stage ('Tests') {
