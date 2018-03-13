14c0c528907e432a2522276c6f29f7abc298c6c7
Admanda Vieira Pires <amanda.pires@mac-17.local>
2018-03-13 18:13:28 -0300

    Changes 4...

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -15,7 +15,7 @@ def call(body) {
                 }
                 stage ('Build') {
                     sh "echo 'building ${config.projectName} ...'"
-                    sh "echo 'variables = ${config.xpto} e ${config.version}'"
+                    sh "echo 'variables = ${config.xpto} + ${config.version}'"
                     sh 'mvn clean install -Dmaven.test.skip=true -Dmaven.javadoc.skip=true'
                 }
                 stage ('Tests') {
