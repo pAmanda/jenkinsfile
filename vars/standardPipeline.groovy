5410dd9738b06a5170abcad0c4d7ca0fe65b8b05
pAmanda <amandoca_779@hotmail.com>
2018-03-14 11:41:44 -0300

    Update standardPipeline.groovy

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -15,7 +15,7 @@ def call(body) {
 
             try {
                 stage ('Clone') {
-                    sh "echo 'Hello...'"
+                    sh "echo 'Ola...'"
                     sh 'printenv'
                     checkout scm
                 }
