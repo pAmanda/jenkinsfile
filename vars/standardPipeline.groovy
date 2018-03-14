741b9233df8902f1e3864d88b5b0ddc75f1aca3c
pAmanda <amandoca_779@hotmail.com>
2018-03-14 11:02:05 -0300

    Update standardPipeline.groovy

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -1,5 +1,7 @@
 def call(body) {
 
+        delegate.setChangelog(true);
+        
         properties([
             durabilityHint('PERFORMANCE_OPTIMIZED')
         ])
@@ -8,7 +10,7 @@ def call(body) {
         body.resolveStrategy = Closure.DELEGATE_FIRST
         body.delegate = config
         body()
-
+      
         node {
             // Clean workspace before doing anything
             deleteDir()
