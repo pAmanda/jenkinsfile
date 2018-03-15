cc585110fe013d0f16dd92037dabb0408bb0549f
Admanda Vieira Pires <amanda.pires@mac-17.local>
2018-03-15 13:27:29 -0300

    Changes

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -3,10 +3,10 @@
         durabilityHint('PERFORMANCE_OPTIMIZED')
     ])
 
-    parameters {
-        string(name: 'version', defaultValue: '0.0.1-SNAPSHOT', description: 'Número da versão que será fechada.')
-        string(name: 'next_version', defaultValue: '0.0.2-SNAPSHOT', description: 'Próxima versão de desenvolvimento.')
-    }
+    // parameters {
+    //     string(name: 'version', defaultValue: '0.0.1-SNAPSHOT', description: 'Número da versão que será fechada.')
+    //     string(name: 'next_version', defaultValue: '0.0.2-SNAPSHOT', description: 'Próxima versão de desenvolvimento.')
+    // }
 
     node {
         // Clean workspace before doing anything
