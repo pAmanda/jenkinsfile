5303b8c0a394ef42b96a1037d4df7b8d379948ce
pAmanda <amandoca_779@hotmail.com>
2018-03-15 13:35:10 -0300

    Update standardPipeline.groovy

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -1,12 +1,13 @@
+def call(body) {
+  
+    properties([
+       durabilityHint('PERFORMANCE_OPTIMIZED')
+    ])
 
-//    properties([
-  //      durabilityHint('PERFORMANCE_OPTIMIZED')
-   // ])
-
-    // parameters {
-    //     string(name: 'version', defaultValue: '0.0.1-SNAPSHOT', description: 'Número da versão que será fechada.')
-    //     string(name: 'next_version', defaultValue: '0.0.2-SNAPSHOT', description: 'Próxima versão de desenvolvimento.')
-    // }
+     parameters {
+         string(name: 'version', defaultValue: '0.0.1-SNAPSHOT', description: 'Número da versão que será fechada.')
+         string(name: 'next_version', defaultValue: '0.0.2-SNAPSHOT', description: 'Próxima versão de desenvolvimento.')
+     }
 
     node {
         // Clean workspace before doing anything
@@ -64,3 +65,4 @@
             throw err
         }
     }
+}
