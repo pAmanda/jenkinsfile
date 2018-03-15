2e6b2deef542392fa7bb16ef8302ab8c0c6464c9
pAmanda <amandoca_779@hotmail.com>
2018-03-15 15:14:14 -0300

    Update standardPipeline.groovy

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -5,7 +5,7 @@ def call(body) {
     ])
     
     parameters {
-        string(name: 'version', defaultValue: '0.0.1-SNAPSHOT', description: 'Número da versão que será fechada.')
+        string(name: 'versgfdfion', defaultValue: '0.0.1-SNAPSHOT', description: 'Número da versão que será fechada.')
         string(name: 'next_version', defaultValue: '0.0.2-SNAPSHOT', description: 'Próxima versão de desenvolvimento.')
     }
    
