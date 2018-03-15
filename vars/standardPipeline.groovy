0a7ccea1956314ae805115439c8bb72569eaf68f
pAmanda <amandoca_779@hotmail.com>
2018-03-15 14:39:46 -0300

    Testing kdlkjsa...

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -47,13 +47,7 @@ def call(body) {
                     echo 'mvn deploy -Dmaven.test.skip=true'
                 }
             }
-            stage ('Release') {
-               
-                parameters{
-                    string(name: 'version', defaultValue: '0.0.1-SNAPSHOT', description: 'Número da versão que será fechada.')
-                    string(name: 'next_version', defaultValue: '0.0.2-SNAPSHOT', description: 'Próxima versão de desenvolvimento.')
-                } 
-        
+            stage ('Release') {       
                 if((env.BRANCH_NAME == "**/master" || en.BRANCH_NAME == "**/hotfix") && ${next_version} != ${version}) {
                     echo 'Initializing Release phase'
                     sh 'mvn -B -Dtag=${project.artifactId}-${project.version} release:prepare -DreleaseVersion=${version} -DdevelopmentVersion=${next_version}'
