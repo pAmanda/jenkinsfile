25bbb2d713c948aad19ca97f1ed4d4356fd153fa
pAmanda <amandoca_779@hotmail.com>
2018-03-16 11:16:49 -0300

    Update standardPipeline.groovy

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -32,14 +32,15 @@ def call(body) {
                 }
             }
             stage('Quality Gate') {
-                if(env.BRANCH_NAME != "**/feature/*") {
+                 echo "Initializing Quality Gate phase"
+               // if(env.BRANCH_NAME != "**/feature/*") {
                     //timeout(time: 1, unit: 'HOURS') {
                       //  def qg = waitForQualityGate()
                        // if (qg.status != 'OK') {
                          //   error "Pipeline aborted due to quality gate failure: ${qg.status}"
                         //}
                     //}
-                }
+               // }
             }
             stage('Archive') {
                 if(env.BRANCH_NAME == "**/master" || en.BRANCH_NAME == "**/hotfix") {
