5f6752a8efa9346a365933234822cc1eacedde92
pAmanda <amandoca_779@hotmail.com>
2018-03-19 08:29:56 -0300

    Update standardPipeline.groovy

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -11,7 +11,7 @@ def call(body) {
         try {
             stage('Checkout') {
                 echo "parameters = ${VERSION} e ${NEXT_VERSION}"
-                echo "BRANCH = ${env.BRANCH_NAME} "
+                echo "BRANCH = " + GIT_BRANCH
                 sh 'printenv'
                 checkout scm
             }
