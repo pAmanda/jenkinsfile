17b25d3db72dfdde0e5049cf4c4135366b2f984f
pAmanda <amandoca_779@hotmail.com>
2018-03-26 12:10:57 -0300

    Update standardPipeline.groovy

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -95,7 +95,6 @@ def call(body) {
                 if(VERSION != NEXT_VERSION) {
                     if(branch_is_master() || branch_is_hotfix()) {
                         echo 'Initializing Docker phase'
-                        sh "docker login -u anderson.feitosa --password-stdin"
                         sh "mvn package docker:build docker:push"
                     }
                 }
