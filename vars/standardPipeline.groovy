0ee785183816cd464da43c3882bef3877ac15bc7
Amanda Vieira Pires <amandoca_779@hotmail.com>
2018-04-05 18:05:31 -0300

    Update standardPipeline.groovy

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -70,7 +70,7 @@ def call(body) {
                     echo "===================================================="
                     echo "Quality Gate Stage"
                     echo "===================================================="
-                    timeout(time: 1, unit: 'HOURS') {
+                    timeout(time: 1, unit: 'MINUTES') {
                         def qg = waitForQualityGate()
                         if (qg.status != 'OK') {
                             error "Pipeline aborted due to quality gate failure: ${qg.status}"
