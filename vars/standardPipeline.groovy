8f63c4bcd408e192f26286d645f2288087b41e16
Amanda Vieira Pires <amandoca_779@hotmail.com>
2018-04-10 18:04:58 -0300

    Update standardPipeline.groovy

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -19,12 +19,14 @@ def call(body) {
         def COMMIT_MESSAGE = sh (script: 'git log -1 --pretty=%B',returnStdout: true).trim()
         
         if (COMMIT_MESSAGE.startsWith("[maven-release-plugin]") 
-            && (env.BRANCH_NAME != '' || VERSION != NEXT_VERSION)) {
+            && ((env.BRANCH_NAME != '' || env.BRANCH_NAME == NULL)
+            || VERSION != NEXT_VERSION)) {
+            
             currentBuild.result = 'SUCCESS'
             echo "Commit message starts with maven-release-plugin. Exiting..."
             
         } else {
-            env.BRANCH_NAME = env.BRANCH_NAME == '' ? get_branch_name(VARS.GIT_BRANCH) : get_branch_name(env.BRANCH_NAME) 
+            env.BRANCH_NAME = (env.BRANCH_NAME == '' || env.BRANCH_NAME == NULL) ?  get_branch_name(VARS.GIT_BRANCH) : get_branch_name(env.BRANCH_NAME) 
             try {
                 stage('Checkout') {
                     echo "===================================================="
