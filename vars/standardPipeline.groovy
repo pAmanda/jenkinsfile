62c19278052dc586bbece02c51006d7a8ed323f9
Amanda Vieira Pires <amanda.pires@cabal.com.br>
2018-11-06 18:51:28 -0200

    Testando 2

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -4,7 +4,7 @@ def call(body) {
         durabilityHint('PERFORMANCE_OPTIMIZED')
     ])
 
-    println VERSION
+    println VARS.VERSION
 
     node {
         // Clean workspace before doing anything        
