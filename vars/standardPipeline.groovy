d290cfe9954f3b90f0d38d1d90971078c6bf1152
Amanda Vieira Pires <amanda.pires@cabal.com.br>
2018-11-06 17:42:09 -0200

    Adicionando outros estágio

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -27,16 +27,79 @@ def call(body) {
         } else {
             env.BRANCH_NAME = (env.BRANCH_NAME == '' || env.BRANCH_NAME == null) ?  get_branch_name(VARS.GIT_BRANCH) : get_branch_name(env.BRANCH_NAME) 
             try {
+ stage('Checkout') {
+                    echo "===================================================="
+                    echo "Checkout Stage"
+                    echo "===================================================="
+                    echo "branch name = " + env.BRANCH_NAME
+                    echo "parameters = " + VERSION + " e " + NEXT_VERSION
+                    sh 'git checkout '+ env.BRANCH_NAME
+                }
+                stage('Build') {
+                    echo "===================================================="
+                    echo "Build Stage"
+                    echo "===================================================="
+                    sh "mvn clean install -Dmaven.test.skip=true -Dmaven.javadoc.skip=true"
+                }
+                stage('Test') {
+                    if(!branch_is_feature()) {
+                        echo "===================================================="
+                        echo "Test Stage"
+                        echo "===================================================="
+                        sh "mvn test"
+                    }
+                }
+                stage ('Analyse') {
+                    if(!branch_is_feature()) {
+                        echo "===================================================="
+                        echo "Analyse Stage"
+                        echo "===================================================="
+                        withSonarQubeEnv('Sonar') {                          
+                            sh "mvn sonar:sonar"
+
+                        }
+                    }
+                }
+                stage('Quality Gate') {
+                     if(!branch_is_feature()) {
+                        // echo "===================================================="
+                        // echo "Quality Gate Stage"
+                        // echo "===================================================="
+			            // timeout(time: 1, unit: 'HOURS') {
+                    	//    def qg = waitForQualityGate()
+			            //     if (qg.status != 'OK') {
+		                //       error "Pipeline aborted due to quality gate failure: ${qg.status}"
+    			        //     } 
+		                // }
+                    }
+                }
+                stage('Archive') {
+                    if(branch_is_master() || branch_is_hotfix()) {
+                        echo "===================================================="
+                        echo "Archive Stage"
+                        echo "===================================================="
+                        sh 'mvn deploy -Dmaven.test.skip=true'
+                    }
+                }
+                stage ('Release') {
+                    if(VERSION != NEXT_VERSION) {
+                        if(branch_is_master() || branch_is_hotfix()) {
+                            echo "===================================================="
+                            echo "Release Stage"
+                            echo "===================================================="
+                            sh 'mvn -B release:prepare -DreleaseVersion=${VERSION} -DdevelopmentVersion=${NEXT_VERSION}'
+                        }
+                    }
+                }
                 stage('Docker') {
                     if(VERSION != NEXT_VERSION) {
                         if(branch_is_master() || branch_is_hotfix()) {
                             echo "===================================================="
                             echo "Docker Stage"
                             echo "===================================================="
-                            openshift.withCluster('openshift-pocose', '14eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJzaXBwZSIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VjcmV0Lm5hbWUiOiJqZW5raW5zLXRva2VuLWNxeHJ0Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQubmFtZSI6ImplbmtpbnMiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC51aWQiOiI2ZmJlNWY0Yy1hN2VkLTExZTgtYmFmYS1hYWQ5ZGI0NzZhMzciLCJzdWIiOiJzeXN0ZW06c2VydmljZWFjY291bnQ6c2lwcGU6amVua2lucyJ9.Z3dFK3IQ3yoTQsyZYfPT0RDvwHmG4pm9BNBBkKdVYOHAwkuULfMzCuGC5vZ9uPDFXFsdcdg2nBMlj66nnhKKaxFeEdoJOf5eyRM0VK1rF0VL9b3fwtd8-UgmvjISgWO8Eous1Gr3uAUkOnw9HA3ioD5CBsz9JIMLhIG7bR0zkPoXcd16gw_Q3RSL_5BMnuFXLIroQtmjfVDjKzZwHK6Q_dcVCXdQMHLpd-yx76ahOiQdRjWmxZ4CxGTr9y8ccCRcECw3TWLcN32bSY3Z5xRVeESXEbBaWeJaeBuJUOfTyMdbJuvagj0xJMrmjsRU1RCAF2nKXYUzzq7lo7u3qulAMg') {
-                                sh "mvn package docker:build docker:push"
-                            }
-                            // sh "mvn package docker:build docker:push"
+                            echo "Fazendo checkout na tag gerada"
+                            sh 'git checkout $(git describe --tags $(git rev-list --tags --max-count=1))'
+                            echo "===================================================="
                         }
                     }
                 }
