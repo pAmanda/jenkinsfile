759086a2088a00fde03d0d47d419f1c31e3a8db1
Amanda Vieira Pires <amanda.pires@cabal.com.br>
2018-11-07 17:07:08 -0200

    Testando novo pipeline

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -4,7 +4,7 @@ def call(body) {
         durabilityHint('PERFORMANCE_OPTIMIZED')
     ])
 
-    if(Test == true) {
+    if(TEST == true) {
         pipeline { 
             agent any
             stages {
