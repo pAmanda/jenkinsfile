07b4aa98f7a7902f09cb0a2c1dedf54a8fef1204
Amanda Vieira Pires <amanda.pires@cabal.com.br>
2018-11-08 19:42:09 -0200

    Alterando fluxo

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -17,6 +17,10 @@ def call(body) {
     } else if(ENVIRONMENT == 'Homologation' || nothing) {
         pipeline { 
             agent any
+            tools {
+                maven 'Maven3'
+                jdk 'jdk1.8' 
+            }
             stages {
                 stage('Checkout') {
                     steps {
@@ -52,6 +56,72 @@ def call(body) {
                         echo "Test Stage"
                         echo "===================================================="
                         sh "mvn test"
+                    }
+                }
+                stage('Analyse') {
+                    when {
+                        not {
+                            expression {
+                               branch_is_feature() 
+                            } 
+                        } 
+                    }
+                    steps {
+                        echo "===================================================="
+                        echo "Analyse Stage"
+                        echo "===================================================="
+                        withSonarQubeEnv('Sonar') {                          
+                            sh "mvn sonar:sonar"
+
+                        }   
+                    }    
+                }
+                stage('Quality Gate') {
+                    when {
+                        not {
+                            expression {
+                               branch_is_feature() 
+                            } 
+                        } 
+                    }
+                    steps {
+                        echo "===================================================="
+                        echo "Quality Gate Stage"
+                        echo "===================================================="
+			            // timeout(time: 1, unit: 'HOURS') {
+                    	//    def qg = waitForQualityGate()
+			            //     if (qg.status != 'OK') {
+		                //       error "Pipeline aborted due to quality gate failure: ${qg.status}"
+    			        //     } 
+		                // }                      
+                    }  
+                }
+                stage('Archive') {
+                    when {
+                        expression {
+                            branch_is_master_hotfix() && different_versions()
+                        } 
+                    }
+                    steps {
+                        echo "===================================================="
+                        echo "Release Stage"
+                        echo "===================================================="
+                        sh 'mvn -B release:prepare -DreleaseVersion=${VERSION} -DdevelopmentVersion=${NEXT_VERSION}'
+                    }
+                }
+                stage('Docker') {
+                    when {
+                        expression {
+                            branch_is_master_hotfix() && different_versions()
+                        } 
+                    }
+                    steps {
+                        echo "===================================================="
+                        echo "Docker Stage"
+                        echo "===================================================="
+                        echo "Fazendo checkout na tag gerada"
+                        sh 'git checkout $(git describe --tags $(git rev-list --tags --max-count=1))'
+                        echo "===================================================="
                     }
                 }
             }
@@ -92,4 +162,12 @@ def Boolean branch_is_hotfix() {
 
 def Boolean test_branch_name(branch) {
     return BRANCH_NAME.startsWith(branch)
+}
+
+def Boolean branch_is_master_hotfix() {
+    return branch_is_master() || branch_is_hotfix()
+}
+
+def Boolean different_versions() {
+    return VERSION != NEXT_VERSION;
 }
