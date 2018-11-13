47f2aeabee668adaa7314f6f49fa1b4b52c4609b
Amanda Vieira Pires <amanda.pires@cabal.com.br>
2018-11-13 18:39:47 -0200

    Testando com o tagName

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -29,9 +29,9 @@ def call(body) {
                         echo "Checkout Stage"
                         echo "===================================================="
                         script {
-                            env.BRANCH_NAME = (env.BRANCH_NAME == '' || env.BRANCH_NAME == null) ?  get_env.BRANCH_NAME(GIT_BRANCH) : get_env.BRANCH_NAME(env.BRANCH_NAME) 
+                            env.BRANCH_NAME = (env.BRANCH_NAME == '' || env.BRANCH_NAME == null) ?  get_branch_name(GIT_BRANCH) : get_branch_name(env.BRANCH_NAME) 
                         }
-                        echo "env.BRANCH_NAME = " + env.BRANCH_NAME
+                        echo "BRANCH_NAME = " + env.BRANCH_NAME
                         echo "PARAMETERS = VERSION: " + VERSION + " e NEXT_VERSION: " + NEXT_VERSION
                         sh 'git checkout ' + env.BRANCH_NAME
                     }
@@ -195,27 +195,27 @@ def call(body) {
     }
 } 
     
-def String get_env.BRANCH_NAME(env.BRANCH_NAME) {
-    return env.BRANCH_NAME.replaceAll("origin/", "").trim();
+def String get_branch_name(branch_name) {
+    return branch_name.replaceAll("origin/", "").trim();
 }
 
 def Boolean branch_is_feature() {
-    return test_env.BRANCH_NAME("feature/")
+    return test_branch_name("feature/")
 }
 
 def Boolean branch_is_master() {
-    return test_env.BRANCH_NAME("master")
+    return test_branch_name("master")
 }
 
 def Boolean branch_is_develop() {
-    return test_env.BRANCH_NAME("develop")
+    return test_branch_name("develop")
 }
 
 def Boolean branch_is_hotfix() {
-    return test_env.BRANCH_NAME("hotfix/")
+    return test_branch_name("hotfix/")
 }
 
-def Boolean test_env.BRANCH_NAME(branch) {
+def Boolean test_branch_name(branch) {
     return env.BRANCH_NAME.startsWith(branch)
 }
 
